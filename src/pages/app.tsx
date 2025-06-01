8ab2d0e33c202dc00848c50f8f1bba47b7f185ad
Siddharth-work <siddharth-work@Sids-Mac.local>
2025-06-01 10:20:09 +0530

    fixed the error

--- src/pages/app.tsx
+++
@@ -1,8 +0,0 @@
-import 'antd/dist/reset.css'; // for AntD v5+
-import '@/styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
